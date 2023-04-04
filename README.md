3dafbef869ec7425aa584fc132e5496342cf068b
Yogesh Yadav <yogeshyadavas2@gmail.com>
2023-04-04 12:43:56 +0530

    first commit dashboard

---
+++ README.md
@@ -0,0 +1,8 @@
+### React Dashboard
+
+Build a COMPLETE React Admin Dashboard App | React, Material UI, Data Grid, Light & Dark Mode
+
+For more similar fallow on [LinkedIn](https://www.linkedin.com/in/yogeshyadavas/)
+
+# [View Demo](https://react-dashboard-yogesh.netlify.app/)
+![react-dashboard](https://github.com/yogeshyadavas/assets/blob/main/project%20image/react-dashboard.png?raw=true)
---
+++ src/App.js
@@ -0,0 +1,51 @@
+import { useState } from "react";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Topbar from "./scenes/global/Topbar";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+import Contacts from "./scenes/contacts";
+import Invoices from "./scenes/invoices";
+import Form from "./scenes/form";
+import Bar from "./scenes/bar";
+import Pie from "./scenes/pie";
+import Line from "./scenes/line";
+import FAQ from "./scenes/faq";
+import Calendar from "./scenes/calendar/Calendar";
+import Geography from "./scenes/geography";
+import Sidebar from "./scenes/global/Sidebar";
+
+function App() {
+  const [theme, colorMode] = useMode();
+  const [isSidebar, setIsSidebar] = useState(true);
+
+  return (
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar isSidebar={isSidebar} />
+          <main className="content">
+            <Topbar setIsSidebar={setIsSidebar} />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/team" element={<Team />} />
+              <Route path="/contacts" element={<Contacts />} />
+              <Route path="/invoices" element={<Invoices />} />
+              <Route path="/form" element={<Form />} />
+              <Route path="/bar" element={<Bar />} />
+              <Route path="/pie" element={<Pie />} />
+              <Route path="/line" element={<Line />} />
+              <Route path="/faq" element={<FAQ />} />
+              <Route path="/calendar" element={<Calendar />} />
+              <Route path="/geography" element={<Geography />} />
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
+  );
+}
+
+export default App;
